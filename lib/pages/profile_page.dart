cee27604e46b637b84e408b40f3076ff2b2fd669
shabiche <thomas2almeida@gmail.com>
2025-03-08 14:01:53 +0100

    v1 edit profile

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,35 +12,121 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  // Déclaration des contrôleurs pour chaque champ
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _firstNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _addressController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
   final TextEditingController _birthDateController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmNewPasswordController =
+      TextEditingController();
   bool _isEditing = false;
+  bool _passwordsMatch = true;
+  String? _oldEmail; // Stocker l'ancienne adresse e-mail
 
   @override
   void initState() {
     super.initState();
-    _loadUserData(); // Charger les données de l'utilisateur dès le démarrage
+    _loadUserData();
   }
 
-  // Fonction pour charger les données depuis SharedPreferences
   Future<void> _loadUserData() async {
     final prefs = await SharedPreferences.getInstance();
-
     setState(() {
-      _nameController.text = prefs.getString('nom') ?? ''; // Nom
-      _firstNameController.text = prefs.getString('prenom') ?? ''; // Prénom
-      _emailController.text = prefs.getString('adresse_mail') ?? ''; // Email
-      _addressController.text = prefs.getString('adresse') ?? ''; // Adresse
-      _phoneController.text = prefs.getString('num_tel') ?? ''; // Téléphone
+      _nameController.text = prefs.getString('nom') ?? '';
+      _firstNameController.text = prefs.getString('prenom') ?? '';
+      _emailController.text = prefs.getString('adresse_mail') ?? '';
+      _oldEmail = _emailController.text; // Sauvegarde de l'ancien email
+      _addressController.text = prefs.getString('adresse') ?? '';
+      _phoneController.text = prefs.getString('num_tel') ?? '';
       _birthDateController.text =
-          prefs.getString('date_naissance') ?? ''; // Date de naissance
+          prefs.getString('date_naissance')?.substring(0, 10) ?? '';
     });
+    debugPrint("Données utilisateur chargées: $_oldEmail");
+  }
+
+  Future<void> _saveProfile(String currentPassword) async {
+    if (_newPasswordController.text.isNotEmpty && !_passwordsMatch) {
+      debugPrint("Erreur: les mots de passe ne correspondent pas");
+
+      return;
+    }
+
+    final prefs = await SharedPreferences.getInstance();
+    int? userId = prefs.getInt('id_t_user');
+    if (userId == null) {
+      debugPrint("Erreur: ID utilisateur introuvable dans SharedPreferences");
+      return;
+    }
+
+    final body = jsonEncode({
+      'nom': _nameController.text,
+      'prenom': _firstNameController.text,
+      'ancienne_adresse_mail': _oldEmail,
+      'adresse_mail': _emailController.text,
+      'adresse': _addressController.text,
+      'num_tel': _phoneController.text,
+      'date_naissance': _birthDateController.text,
+      'current_password': currentPassword,
+      'new_password': _newPasswordController.text.isNotEmpty
+          ? _newPasswordController.text
+          : null,
+    });
+
+    debugPrint("Envoi des données au serveur: $body id: $userId");
+
+    final response = await http.put(
+      Uri.parse('http://localhost:3000/api/users/$userId'),
+      headers: {'Content-Type': 'application/json'},
+      body: body,
+    );
+
+    debugPrint("Réponse du serveur: ${response.statusCode} - ${response.body}");
+
+    if (response.statusCode == 200) {
+      setState(() => _isEditing = false);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Profil mis à jour avec succès!')),
+      );
+    } else {
+      setState(() {
+        _showPasswordDialog(error: true);
+      });
+    }
+  }
+
+  void _showPasswordDialog({bool error = false}) {
+    TextEditingController passwordController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Vérification du mot de passe'),
+        content: TextField(
+          controller: passwordController,
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: 'Mot de passe actuel',
+            errorText: error ? 'Mot de passe incorrect' : null,
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Annuler'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              debugPrint("Mot de passe saisi: ${passwordController.text}");
+
+              _saveProfile(passwordController.text);
+            },
+            child: const Text('Confirmer'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -55,25 +143,26 @@ class _ProfilePageState extends State<ProfilePage> {
               ),
             ),
             const SizedBox(height: 20),
-            // Nom
             _buildProfileField('Nom', _nameController),
             _buildProfileField('Prénom', _firstNameController),
             _buildProfileField('Email', _emailController),
             _buildProfileField('Adresse', _addressController),
             _buildProfileField('Téléphone', _phoneController),
             _buildProfileField('Date de naissance', _birthDateController),
-            // Ajouter le champ mot de passe seulement en mode édition
-            if (_isEditing)
-              _buildProfileField('Mot de passe', _passwordController,
-                  isPassword: true),
+            if (_isEditing) ...[
+              _buildPasswordField(
+                  'Nouveau mot de passe', _newPasswordController),
+              _buildPasswordField('Confirmer le nouveau mot de passe',
+                  _confirmNewPasswordController),
+            ],
             const SizedBox(height: 20),
-
-            // Bouton Modifier le profil
             ElevatedButton.icon(
               onPressed: () {
-                setState(() {
-                  _isEditing = !_isEditing;
-                });
+                if (_isEditing) {
+                  _showPasswordDialog();
+                } else {
+                  setState(() => _isEditing = true);
+                }
               },
               icon: const Icon(Icons.edit),
               label: Text(
@@ -85,38 +174,61 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-  // Fonction pour afficher un champ de profil modifiable ou non
-  Widget _buildProfileField(String label, TextEditingController controller,
-      {bool isPassword = false}) {
+  Widget _buildProfileField(String label, TextEditingController controller) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            label,
-            style: const TextStyle(
-                fontSize: 14, fontWeight: FontWeight.bold, color: Colors.grey),
-          ),
+          Text(label,
+              style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey)),
           const SizedBox(height: 4),
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: Colors.grey[200],
-              borderRadius: BorderRadius.circular(8),
+          TextField(
+            controller: controller,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              filled: true,
+              fillColor: Color.fromARGB(255, 236, 242, 246),
             ),
-            child: _isEditing
-                ? TextField(
-                    controller: controller,
-                    obscureText:
-                        isPassword, // Masquer le texte si c'est un mot de passe
-                    decoration: const InputDecoration(border: InputBorder.none),
-                  )
-                : Text(
-                    controller.text,
-                    style: const TextStyle(fontSize: 16),
-                  ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPasswordField(String label, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(label,
+              style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey)),
+          const SizedBox(height: 4),
+          TextField(
+            controller: controller,
+            obscureText: true,
+            onChanged: (value) {
+              setState(() {
+                _passwordsMatch = _newPasswordController.text ==
+                    _confirmNewPasswordController.text;
+              });
+            },
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              filled: true,
+              fillColor: Colors.grey[200],
+              errorText: (!_passwordsMatch &&
+                      controller == _confirmNewPasswordController)
+                  ? 'Les mots de passe ne correspondent pas'
+                  : null,
+            ),
           ),
         ],
       ),
